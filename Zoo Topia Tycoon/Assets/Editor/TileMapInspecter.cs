75870a6303f94076c2399fb4e962d66540ba0e99
Wasted-Art <wastedartz@gmail.com>
2019-06-18 00:45:42 +0200

    Genskabelse af det der gik tabt
    fortsæt herfra
    https://youtu.be/gTHKhE5s3W8?list=PLbghT7MmckI4qGA0Wm_TZS8LVrqS47I9R&t=556

---
+++ Zoo Topia Tycoon/Assets/Editor/TileMapInspecter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CustomEditor(typeof(TileMap))]
+public class TileMapInspecter : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        DrawDefaultInspector();
+        if (GUILayout.Button("Regenerate"))
+        {
+            TileMap tileMap = (TileMap)target;
+            tileMap.BuildMesh();
+        }
+    }
+}
