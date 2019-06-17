6002226d5c649d64fdfcf6fbcc8a932e2cfd9f1b
Wasted-Art <wastedartz@gmail.com>
2019-06-17 19:06:32 +0200

    fuck up

---
+++ Zoo Topia Tycoon/Assets/TileGraphics/TGMap.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+[RequireComponent (typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class TGMap : MonoBehaviour {
+
+    //Variabler
+    public int size_x = 50;
+    public int size_z = 50;
+    public float tileSize = 1.0f;
+
+    public Texture2D terrainTiles;
+    public int tileResolution;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        BuildMesh();
+	}
+
+    Color[][] ChopUpTiles()
+    {
+        int numTilesPerRow = terrainTiles.width / tileResolution;
+        int numRows = terrainTiles.height / tileResolution;
+
+        Color[][] tiles = new Color[numTilesPerRow * numRows][];
+
+        for (int y = 0; y < numRows; y++)
+        {
+            for (int x = 0; x < numTilesPerRow; x++)
+            {
+                tiles[y * numTilesPerRow + x] = terrainTiles.GetPixels(x * tileResolution, y * tileResolution, tileResolution, tileResolution);
+            }
+        }
+        return tiles;
+    }
+
+    void BuildTexture()
+    {
+        int texWidth = size_x * tileResolution;
+        int texHeight = size_z * tileResolution;
+        Texture2D texture = new Texture2D(texWidth, texHeight);
+
+        Color[][] tiles = ChopUpTiles();
+
+        for (int y = 0; y < size_z; y++)
+        {
+            for (int x = 0; x < size_x; x++)
+            {
+                Color[] p = tiles[Random.Range(0, 4)];
+                texture.SetPixels(x * tileResolution, y * tileResolution, tileResolution, tileResolution, p);
+                /*dette kan være brugbart når der skal benyttes standart grundere
+                Color c = new Color( Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+                texture.SetPixel(x, y, c);*/
+            }
+        }
+        //For blurred texture brug Bilinear istedet for Point
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.Apply();
+        MeshRenderer mesh_Renderer = GetComponent<MeshRenderer>();
+        //sharedMaterial i editor, og material i gameplay
+        mesh_Renderer.sharedMaterials[0].mainTexture = texture;
+
+        Debug.Log("Done TEXTURE!");
+    }
+
+
+    public void BuildMesh()
+    {
+        int numTiles = size_x * size_z;
+        int numTris = numTiles * 2;
+
+        int vsize_x = size_x + 1;
+        int vsize_z = size_z + 1;
+        int numVerts = vsize_x * vsize_z;
+
+
+        //Generate the mesh data
+        Vector3[] vertices = new Vector3[numVerts];
+        Vector3[] normals = new Vector3[numVerts];
+        Vector2[] uv = new Vector2[numVerts];
+
+        int[] triangles = new int[numTris * 3];
+
+        int x, z;
+        for (z = 0; z < vsize_z; z++)
+        {
+            for (x = 0; x < vsize_x; x++)
+            {
+                //Random.Range(-1f, 1f)
+                vertices[z * vsize_x + x] = new Vector3(x * tileSize, Random.Range(-1f, 1f), z * tileSize);
+                normals[z * vsize_x + x] = Vector3.up;
+                uv[z * vsize_x + x] = new Vector2( (float)x / size_x, (float)z / size_z);
+            }
+        }
+
+        for (z = 0; z < size_z; z++)
+        {
+            for (x = 0; x < size_x; x++)
+            {
+                //Debug.Log("Triangle" + x + ", " + z);
+                int squareIndex = z * size_x + x;
+                int triOffset = squareIndex * 6;
+                triangles[triOffset + 0] = z * vsize_x + x + 0;
+                triangles[triOffset + 1] = z * vsize_x + x + vsize_x + 0;
+                triangles[triOffset + 2] = z * vsize_x + x + vsize_x + 1;
+
+                triangles[triOffset + 3] = z * vsize_x + x + 0;
+                triangles[triOffset + 4] = z * vsize_x + x + vsize_x + 1;
+                triangles[triOffset + 5] = z * vsize_x + x + 1;
+            }
+        }
+
+
+                //Create new Mesh and populate it with data
+                Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+
+        //Assign our mesh to our filter/renderer/collider
+        MeshFilter mesh_filter = GetComponent<MeshFilter>();
+        MeshRenderer mesh_renderer = GetComponent<MeshRenderer>();
+        MeshCollider mesh_collider = GetComponent<MeshCollider>();
+
+        mesh_filter.mesh = mesh;
+        mesh_collider.sharedMesh = mesh;
+
+        BuildTexture();
+    }
+}
